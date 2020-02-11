4078c86788990af985d3ac7f461f0668d400ad4b
bkraemer5 <bkraemer5@csu.fullerton.edu>
2020-02-11 10:06:42 -0800

    added method to check for separators and operators

--- lexer.cpp
+++ lexer.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <cctype>
 
 using namespace std;
 
@@ -21,11 +22,47 @@ int main() {
 	ifstream sourceCode;
 	sourceCode.open("sourceCode.txt");
 	string line;
-	string::iterator i;
+	string identifier;
+	string number;
 	while (!sourceCode.eof()) {
 		getline(sourceCode, line);
-	}
+		for (string::iterator i = line.begin(); i != line.end(); i++) {
+			// checks if character is alphabetic
+			if (isalpha(*i)) {
+				identifier = identifier + *i;
+			}
+			// checks if character is a digit
+			else if (isdigit(*i)) {
+				if (identifier.compare("") == 0) {
+					number += *i;
+				}
+				else {
+					identifier = identifier + *i;
+				}
+				cout << *i << " ";
+			}
 
+			// checks if character is a separator
+			// this ends up resetting the identifier
+			for (vector<char>::iterator s = separators.begin(); s != separators.end(); s++) {
+				if (*s == *i) {
+					cout << identifier << " ";
+					identifier = "";
+					cout << *s;
+				}
+			}
+
+			//checks if character is an operator
+			for (vector<char>::iterator o = operators.begin(); o != operators.end(); o++) {
+				if (*o == *i) {
+					cout << identifier << " ";
+					identifier = "";
+					cout << *o << " ";
+				}
+			}
+		}
+		cout << endl;
+	}
 
 	return 0;
 }
